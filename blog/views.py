d8065af5bf96e07433678549870a08e91f822a0f
Raniere Silva <raniere@rgaiacs.com>
2019-08-08 15:56:24 -0300

    Create view for single post

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def index(request):
     return render(
         request,
         'blog/index.html',
         {}
+    )
+
+def blog(request, title):
+    post = Post.objects.get(
+        title__iexact=title
+    )
+
+    print(post)
+
+    return render(
+        request,
+        'blog/post.html',
+        {
+            "post": post,
+        }
     )
